5aa8cef05fb948c5732d0bcaaf4b4366f453fe9c
sandy ambarita <>
2020-11-16 14:09:06 +0700

    first commit

---
+++ ListFromJsonSwiftUI/DetailPokemonView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailPokemonView.swift
+//  ListFromJsonSwiftUI
+//
+//  Created by sandy ambarita on 16/11/20.
+//
+
+import SwiftUI
+
+struct DetailPokemonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailPokemonView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailPokemonView()
+    }
+}
---
+++ ListFromJsonSwiftUI/NetworkingManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkingManager.swift
+//  ListFromJsonSwiftUI
+//
+//  Created by sandy ambarita on 16/11/20.
+//
+
+import Foundation
